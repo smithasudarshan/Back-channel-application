766c47724589423d472b514c131b653e49b93e0d
Smitha Sudarshan <kkalker@ncsu.edu>
2011-10-07 13:42:32 -0400

    commit msg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSC517Project1BackChannelApp::Application.config.secret_token = 'c9a1dbb68d7eefa8f3498096793f717d9b3f70459a95f669eb63797247653d86aab5edde7f690528fcf125527ea37d06b2605b5fcde61511a1c7343c0b80ab08'
